db5a2e81e3185adfb80a9a94c6c0d55c6c5dfcaa
unknown <jzzthx@gmail.com>
2022-06-26 02:05:56 -0500

    probando heroku

--- AppRestaurant/Controllers/BaseController.cs
+++ AppRestaurant/Controllers/BaseController.cs
@@ -1,8 +1,12 @@
 ﻿using AppRestaurant.Repository.Repository.Implement;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppRestaurant.Controllers
 {
+    [Produces("application/json")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
     public class BaseController : ControllerBase
     {
         protected IUnitOfWork _unit;
