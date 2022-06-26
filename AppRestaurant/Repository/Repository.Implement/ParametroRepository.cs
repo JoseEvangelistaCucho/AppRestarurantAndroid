9c208e4c1afb30153591e91daeb503c6c64e62a0
unknown <jzzthx@gmail.com>
2022-06-26 11:01:44 -0500

    Cambios en parametros

--- AppRestaurant/Repository/Repository.Implement/ParametroRepository.cs
+++ AppRestaurant/Repository/Repository.Implement/ParametroRepository.cs
@@ -7,5 +7,10 @@ namespace AppRestaurant.Repository.Repository.Implement
         public ParametroRepository(string connectionString) : base(connectionString)
         {
         }
+
+        public ResponseHeader GetByDocumento()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
